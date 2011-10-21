80159b8655a4f20fa382248d475ed00f147a23a2
wolfgang.schwitzer <wolfgang.schwitzer@824f5622-b1fa-dc22-659f-56585b84b2b6>
2011-10-21 23:30:46 +0000

    Added javadoc.

--- trunk/edu.tum.cs.cadmos.commons/src/edu/tum/cs/cadmos/commons/IIdentifiable.java
+++ trunk/edu.tum.cs.cadmos.commons/src/edu/tum/cs/cadmos/commons/IIdentifiable.java
@@ -1,7 +1,44 @@
+/*--------------------------------------------------------------------------+
+|                                                                          |
+| Copyright 2008-2012 Technische Universitaet Muenchen                     |
+|                                                                          |
+| Licensed under the Apache License, Version 2.0 (the "License");          |
+| you may not use this file except in compliance with the License.         |
+| You may obtain a copy of the License at                                  |
+|                                                                          |
+|    http://www.apache.org/licenses/LICENSE-2.0                            |
+|                                                                          |
+| Unless required by applicable law or agreed to in writing, software      |
+| distributed under the License is distributed on an "AS IS" BASIS,        |
+| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+| See the License for the specific language governing permissions and      |
+| limitations under the License.                                           |
++--------------------------------------------------------------------------*/
+
 package edu.tum.cs.cadmos.commons;
 
+/**
+ * An object that is identified by a unique <i>id</i>.
+ * <p>
+ * IIdentifieable objects have the following conventions:
+ * <ul>
+ * <li>The <i>id</i> must not be <code>null</code>.
+ * <li>Equality is given by <i>id</i>s ({@link #equals(Object)} is implemented
+ * accordingly).
+ * <li>The hashcode is given by the <i>id</i>'s hashcode ({@link #hashCode()} is
+ * implemented accordingly).
+ * </ul>
+ * 
+ * @see AbstractIdentifiable
+ * 
+ * @author wolfgang.schwitzer
+ * @author $Author$
+ * @version $Rev$
+ * @ConQAT.Rating GREEN Hash: 02F6400DE690FE84BC156BD1707F67AB
+ */
 public interface IIdentifiable {
 
+	/** Returns the unique id of this object. */
 	String getId();
 
 }
