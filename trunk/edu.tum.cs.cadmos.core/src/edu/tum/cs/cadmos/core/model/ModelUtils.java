7e797a4a2f377ee50d8d8d5d9b6588f4130dd920
wolfgang.schwitzer <wolfgang.schwitzer@824f5622-b1fa-dc22-659f-56585b84b2b6>
2011-10-25 14:04:46 +0000

    ModelUtils now support getOutboundChildChannel(...) and getPathToTransitiveSrc(...).

--- trunk/edu.tum.cs.cadmos.core/src/edu/tum/cs/cadmos/core/model/ModelUtils.java
+++ trunk/edu.tum.cs.cadmos.core/src/edu/tum/cs/cadmos/core/model/ModelUtils.java
@@ -1,24 +1,56 @@
+/*--------------------------------------------------------------------------+
+|                                                                          |
+| Copyright 2008-2012 Technische Universitaet Muenchen                     |
+|                                                                          |
+| Licensed under the Apache License, Version 2.0 (the "License");          |
+| you may not use this file except in compliance with the License.         |
+| You may obtain a copy of the License at                                  |
+|                                                                          |
+|    http://www.apache.org/licenses/LICENSE-2.0                            |
+|                                                                          |
+| Unless required by applicable law or agreed to in writing, software      |
+| distributed under the License is distributed on an "AS IS" BASIS,        |
+| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+| See the License for the specific language governing permissions and      |
+| limitations under the License.                                           |
++--------------------------------------------------------------------------*/
+
 package edu.tum.cs.cadmos.core.model;
 
+import static edu.tum.cs.cadmos.commons.Assert.assertNotNull;
 import static edu.tum.cs.cadmos.commons.Assert.assertTrue;
 import static java.util.Arrays.asList;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Deque;
+import java.util.LinkedList;
 
 import edu.tum.cs.cadmos.commons.IListSet;
 import edu.tum.cs.cadmos.commons.ListSet;
 
+/**
+ * A set of utility methods for operations on classes in the <span
+ * style="font-variant:small-caps">Cadmos</span> core model package.
+ * 
+ * @author wolfgang.schwitzer
+ * @author $Author$
+ * @version $Rev$
+ * @ConQAT.Rating RED Hash:
+ */
 public class ModelUtils {
 
-	public static List<IAtomicComponent> getAtomicComponents(
+	/**
+	 * Returns a set of all atomic components recursively found in the given
+	 * <i>component</i> in DFS order or the <i>component</i> itself if it is
+	 * already an atomic component.
+	 */
+	public static IListSet<IAtomicComponent> getAtomicComponents(
 			IComponent component) {
 		if (component instanceof IAtomicComponent) {
-			return asList((IAtomicComponent) component);
+			return new ListSet<>((IAtomicComponent) component);
 		}
 		assertTrue(component instanceof ICompositeComponent,
 				"Expected ICompositeComponent, but was '%s'", component);
-		final List<IAtomicComponent> result = new ArrayList<>();
+		final ListSet<IAtomicComponent> result = new ListSet<>();
 		for (final IComponent child : ((ICompositeComponent) component)
 				.getChildren()) {
 			result.addAll(getAtomicComponents(child));
@@ -26,13 +58,83 @@ public class ModelUtils {
 		return result;
 	}
 
+	/**
+	 * Returns an outbound channel of any of the children of the given
+	 * <i>parent</i>, equal to the given <i>channel</i> or <code>null</code> if
+	 * no such outbound channel exists.
+	 * <p>
+	 * For the returned channel <code>c</code> either
+	 * <code>(c.getDst() == null) && c.equals(channel) && (c.getSrc().getParent() == parent)</code>
+	 * holds or <code>c == null</code>.
+	 */
+	public static IChannel getOutboundChildChannel(ICompositeComponent parent,
+			IChannel channel) {
+		for (final IComponent child : parent.getChildren()) {
+			final IChannel candidate = child.getOutgoing().get(channel);
+			if (candidate != null && candidate.getDst() == null) {
+				return candidate;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the path of channels to the atomic source component of the given
+	 * <i>channel</i> that is transitively reachable within the given
+	 * <i>systemBoundary</i> or throws an {@link AssertionError} if no such
+	 * source exists.
+	 * <p>
+	 * The path is returned as a {@link Deque} of {@link IChannel}s with the
+	 * channel going out of the transitive source as first element and the given
+	 * <i>channel</i> as last element.
+	 * 
+	 * @throws AssertionError
+	 *             if the component model is no well-formed and, hence, no
+	 *             source component can be reached within the system boundary.
+	 */
+	public static Deque<IChannel> getPathToTransitiveSrc(IChannel channel,
+			ICompositeComponent systemBoundary) {
+		final IComponent src = channel.getSrc();
+		if (src instanceof IAtomicComponent) {
+			return new LinkedList<>(asList(channel));
+		}
+		if (src instanceof ICompositeComponent) {
+			final IChannel srcOutboundChannel = getOutboundChildChannel(
+					(ICompositeComponent) src, channel);
+			assertNotNull(srcOutboundChannel, "srcOutboundChannel");
+			final Deque<IChannel> pathToTransitiveSrc = getPathToTransitiveSrc(
+					srcOutboundChannel, systemBoundary);
+			pathToTransitiveSrc.addLast(channel);
+			return pathToTransitiveSrc;
+		}
+		final IComponent dst = channel.getDst();
+		assertNotNull(dst, "dst");
+		final ICompositeComponent parent = dst.getParent();
+		if (src == null && (parent == systemBoundary || parent == null)) {
+			return new LinkedList<>(asList(channel));
+		}
+		if (src == null && parent != systemBoundary) {
+			final IChannel parentIncomingChannel = parent.getIncoming().get(
+					channel);
+			assertNotNull(parentIncomingChannel, "parentIncomingChannel");
+			final Deque<IChannel> pathToTransitiveSrc = getPathToTransitiveSrc(
+					parentIncomingChannel, systemBoundary);
+			pathToTransitiveSrc.addLast(channel);
+			return pathToTransitiveSrc;
+		}
+		throw new AssertionError("Cannot find transitive source for channel '"
+				+ channel + "' and system boundary '" + systemBoundary + "'");
+	}
+
 	public static IListSet<IComponent> analyzeFlatAtomicComponentNetwork(
-			ICompositeComponent component) {
+			ICompositeComponent systemBoundary) {
 		/* Clone the network of children components. */
 		final IListSet<IComponent> network = new ListSet<>();
-		for (final IComponent child : component.getChildren()) {
+		for (final IComponent child : systemBoundary.getChildren()) {
 			network.add(child.clone(null));
 		}
+		/* Rewire. */
+
 		// /* Rewire the channels connected to the children components. */
 		// for (final IComponent child : network) {
 		// for (final IChannel c : child.getIncoming()) {
--- trunk/edu.tum.cs.cadmos.core/test-src/edu/tum/cs/cadmos/core/model/TestModelUtils.java
+++ trunk/edu.tum.cs.cadmos.core/test-src/edu/tum/cs/cadmos/core/model/TestModelUtils.java
@@ -1,11 +1,14 @@
 package edu.tum.cs.cadmos.core.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
-import java.util.List;
+import java.util.Deque;
 
 import org.junit.Test;
+
+import edu.tum.cs.cadmos.commons.IListSet;
 
 public class TestModelUtils {
 
@@ -17,12 +20,73 @@ public class TestModelUtils {
 		final IAtomicComponent a1 = new AtomicComponent("a1", comp1);
 		final IAtomicComponent a2 = new AtomicComponent("a2", comp2);
 		final IAtomicComponent a3 = new AtomicComponent("a3", comp2);
-		final List<IAtomicComponent> components = ModelUtils
+		final IListSet<IAtomicComponent> components = ModelUtils
 				.getAtomicComponents(root);
 		assertEquals(3, components.size());
 		assertTrue(components.contains(a1));
 		assertTrue(components.contains(a2));
 		assertTrue(components.contains(a3));
+	}
+
+	@Test
+	public void test_getOutboundChildChannel() {
+		final ICompositeComponent parent = new CompositeComponent("parent",
+				null);
+		final IAtomicComponent c1 = new AtomicComponent("c1", parent);
+		final IAtomicComponent c2 = new AtomicComponent("c2", parent);
+		@SuppressWarnings("unused")
+		final IChannel x_internal = new Channel("x", c1, c2, 0);
+		final IChannel x_outbound = new Channel("x", c2, null, 0);
+		final IChannel result = ModelUtils.getOutboundChildChannel(parent,
+				x_outbound);
+		assertSame(x_outbound, result);
+	}
+
+	@Test
+	public void test_getPathToTransitiveSrc() {
+		final ICompositeComponent root = new CompositeComponent("root", null);
+		final ICompositeComponent comp1 = new CompositeComponent("comp1", root);
+		final ICompositeComponent comp2 = new CompositeComponent("comp2", root);
+		final IAtomicComponent a1 = new AtomicComponent("a1", comp1);
+		final IAtomicComponent a2 = new AtomicComponent("a2", comp2);
+		final IAtomicComponent a3 = new AtomicComponent("a3", comp2);
+		final IChannel a_comp1 = new Channel("a", null, comp1, 0);
+		final IChannel a_a1 = new Channel("a", null, a1, 0);
+		final IChannel x_a1 = new Channel("x", a1, null, 0);
+		final IChannel x_comp1_comp2 = new Channel("x", comp1, comp2, 0);
+		final IChannel x_a2 = new Channel("x", null, a2, 0);
+		final IChannel y_a2_a3 = new Channel("y", a2, a3, 0);
+		final IChannel z_a3 = new Channel("z", a3, null, 0);
+		final IChannel z_comp2 = new Channel("z", comp2, null, 0);
+
+		final Deque<IChannel> path1 = ModelUtils.getPathToTransitiveSrc(x_a2,
+				root);
+		assertSame(x_a1, path1.pollFirst());
+		assertSame(x_comp1_comp2, path1.pollFirst());
+		assertSame(x_a2, path1.pollFirst());
+		assertTrue(path1.isEmpty());
+
+		final Deque<IChannel> path2 = ModelUtils.getPathToTransitiveSrc(
+				y_a2_a3, root);
+		assertSame(y_a2_a3, path2.pollFirst());
+		assertTrue(path2.isEmpty());
+
+		final Deque<IChannel> path3 = ModelUtils.getPathToTransitiveSrc(
+				z_comp2, root);
+		assertSame(z_a3, path3.pollFirst());
+		assertSame(z_comp2, path3.pollFirst());
+		assertTrue(path3.isEmpty());
+
+		final Deque<IChannel> path4 = ModelUtils.getPathToTransitiveSrc(a_a1,
+				root);
+		assertSame(a_comp1, path4.pollFirst());
+		assertSame(a_a1, path4.pollFirst());
+		assertTrue(path4.isEmpty());
+
+		final Deque<IChannel> path5 = ModelUtils.getPathToTransitiveSrc(x_a2,
+				comp2);
+		assertSame(x_a2, path5.pollFirst());
+		assertTrue(path5.isEmpty());
 	}
 
 	@Test
