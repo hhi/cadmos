afc8441a750b1d4e89f132aec8c088db6cbd62a3
dominik.chessa@gmail.com <dominik.chessa@gmail.com@824f5622-b1fa-dc22-659f-56585b84b2b6>
2011-10-26 09:12:01 +0000

    Matlab Simulink Utils

--- edu.tum.cs.cadmos.matlab/src/edu/tum/cs/cadmos/matlab/MatlabSimulinkTranslation.java
+++ edu.tum.cs.cadmos.matlab/src/edu/tum/cs/cadmos/matlab/MatlabSimulinkTranslation.java
@@ -1,6 +1,24 @@
 package edu.tum.cs.cadmos.matlab;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.tum.cs.cadmos.core.model.AbstractComponent;
+import edu.tum.cs.cadmos.core.model.IComponent;
+import edu.tum.cs.simulink.model.SimulinkBlock;
 
 public class MatlabSimulinkTranslation {
+
+	public static Map<SimulinkBlock, IComponent> buildGenericMap(SimulinkBlock b) {
+
+		Map<SimulinkBlock, AbstractComponent> map = new HashMap<>();
+
+		if (b.hasSubBlocks()) {
+
+		}
+
+		return null;
+
+	}
 
 }
