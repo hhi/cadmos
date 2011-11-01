5d791ac9862a7d82c4e85f065231749a61259714
wolfgang.schwitzer <wolfgang.schwitzer@824f5622-b1fa-dc22-659f-56585b84b2b6>
2011-11-01 17:00:41 +0000

    Introduced AbstractNumericType as base class for IntegerType and RealType to reduce redundancy.

--- trunk/edu.tum.cs.cadmos.core/src/edu/tum/cs/cadmos/core/types/IntegerType.java
+++ trunk/edu.tum.cs.cadmos.core/src/edu/tum/cs/cadmos/core/types/IntegerType.java
@@ -4,28 +4,14 @@ import static java.math.BigInteger.valueOf;
 
 import java.math.BigInteger;
 
-public class IntegerType extends AbstractType {
-
-	private final BigInteger min;
-
-	private final BigInteger max;
+public class IntegerType extends AbstractNumericType<BigInteger> {
 
 	public IntegerType(BigInteger min, BigInteger max) {
-		super(EType.INTEGER);
-		this.min = min;
-		this.max = max;
+		super(EType.INTEGER, min, max);
 	}
 
 	public IntegerType() {
 		this(valueOf(Integer.MIN_VALUE), valueOf(Integer.MAX_VALUE));
-	}
-
-	public BigInteger getMin() {
-		return min;
-	}
-
-	public BigInteger getMax() {
-		return max;
 	}
 
 }
--- trunk/edu.tum.cs.cadmos.core/src/edu/tum/cs/cadmos/core/types/RealType.java
+++ trunk/edu.tum.cs.cadmos.core/src/edu/tum/cs/cadmos/core/types/RealType.java
@@ -4,28 +4,14 @@ import static java.math.BigDecimal.valueOf;
 
 import java.math.BigDecimal;
 
-public class RealType extends AbstractType {
-
-	private final BigDecimal min;
-
-	private final BigDecimal max;
+public class RealType extends AbstractNumericType<BigDecimal> {
 
 	public RealType(BigDecimal min, BigDecimal max) {
-		super(EType.REAL);
-		this.min = min;
-		this.max = max;
+		super(EType.REAL, min, max);
 	}
 
 	public RealType() {
 		this(valueOf(-Float.MAX_VALUE), valueOf(Float.MAX_VALUE));
-	}
-
-	public BigDecimal getMin() {
-		return min;
-	}
-
-	public BigDecimal getMax() {
-		return max;
 	}
 
 }
