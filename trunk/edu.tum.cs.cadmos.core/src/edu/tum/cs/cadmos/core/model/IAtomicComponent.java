c95d783c8942b8b79d8fac9c82dc3f904fc9b9e9
wolfgang.schwitzer <wolfgang.schwitzer@824f5622-b1fa-dc22-659f-56585b84b2b6>
2011-11-26 20:38:35 +0000

    Added javadoc.

--- trunk/edu.tum.cs.cadmos.core/src/edu/tum/cs/cadmos/core/model/IAtomicComponent.java
+++ trunk/edu.tum.cs.cadmos.core/src/edu/tum/cs/cadmos/core/model/IAtomicComponent.java
@@ -3,10 +3,28 @@ package edu.tum.cs.cadmos.core.model;
 import edu.tum.cs.cadmos.commons.core.IListSet;
 import edu.tum.cs.cadmos.core.machines.IMachine;
 
+/**
+ * An atomic component in a hierarchical data-flow network defines its behavior
+ * by a machine that operates over a set of variables and ports.
+ * <p>
+ * A reference implementation of this interface is the {@link AtomicComponent}
+ * class.
+ * 
+ * @author wolfgang.schwitzer
+ * @version $Rev$
+ * @version $Author$
+ * @version $Date$
+ * @ConQAT.Rating RED Hash:
+ * 
+ * @see AtomicComponent
+ * @see ICompositeComponent
+ */
 public interface IAtomicComponent extends IComponent {
 
+	/** Returns the variables that are in the scope of this atomic component. */
 	IListSet<IVariable> getVariables();
 
+	/** Returns the machine that defines the behavior of this atomic component. */
 	IMachine getMachine();
 
 }
