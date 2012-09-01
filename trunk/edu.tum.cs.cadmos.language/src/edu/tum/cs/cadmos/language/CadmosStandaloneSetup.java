f7bf66944818ea1361b7829017de787f98bd3852
wolfgang.schwitzer@gmail.com <wolfgang.schwitzer@gmail.com@824f5622-b1fa-dc22-659f-56585b84b2b6>
2012-09-01 17:12:10 +0000

    Initial import.

---
+++ trunk/edu.tum.cs.cadmos.language/src/edu/tum/cs/cadmos/language/CadmosStandaloneSetup.java
@@ -0,0 +1,14 @@
+
+package edu.tum.cs.cadmos.language;
+
+/**
+ * Initialization support for running Xtext languages 
+ * without equinox extension registry
+ */
+public class CadmosStandaloneSetup extends CadmosStandaloneSetupGenerated{
+
+	public static void doSetup() {
+		new CadmosStandaloneSetup().createInjectorAndDoEMFRegistration();
+	}
+}
+
