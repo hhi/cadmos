2e3c11b0eb6a27a21057d417bc88d380bef1f47f
wolfgang.schwitzer@gmail.com <wolfgang.schwitzer@gmail.com@824f5622-b1fa-dc22-659f-56585b84b2b6>
2012-09-02 23:27:29 +0000

    Added basic editor callbacks for text selection tracking and model updates.

--- trunk/edu.tum.cs.cadmos.language.ui/src/edu/tum/cs/cadmos/language/ui/CadmosUiModule.java
+++ trunk/edu.tum.cs.cadmos.language.ui/src/edu/tum/cs/cadmos/language/ui/CadmosUiModule.java
@@ -4,12 +4,23 @@
 package edu.tum.cs.cadmos.language.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.IXtextEditorCallback;
+
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
 
 /**
  * Use this class to register components to be used within the IDE.
  */
-public class CadmosUiModule extends edu.tum.cs.cadmos.language.ui.AbstractCadmosUiModule {
+public class CadmosUiModule extends
+		edu.tum.cs.cadmos.language.ui.AbstractCadmosUiModule {
 	public CadmosUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+	}
+
+	public void configureCadmosXtextEditorCallback(Binder binder) {
+		binder.bind(IXtextEditorCallback.class)
+				.annotatedWith(Names.named("CadmosXtextEditorCallback"))
+				.to(CadmosXtextEditorCallback.class);
 	}
 }
