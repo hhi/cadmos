011d5bc745ef16ea46c85d34240f829a93e66d29
wolfgang.schwitzer@gmail.com <wolfgang.schwitzer@gmail.com@824f5622-b1fa-dc22-659f-56585b84b2b6>
2012-09-03 22:41:33 +0000

    Introduced first test of the ArchitectureTranslator based on nodes.

--- trunk/edu.tum.cs.cadmos.language.ui/src/edu/tum/cs/cadmos/language/ui/CadmosXtextModelSelectionChangedListener.java
+++ trunk/edu.tum.cs.cadmos.language.ui/src/edu/tum/cs/cadmos/language/ui/CadmosXtextModelSelectionChangedListener.java
@@ -4,6 +4,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.ILeafNode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
@@ -11,6 +12,10 @@ import org.eclipse.xtext.parser.IParseResult;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
+
+import edu.tum.cs.cadmos.analysis.ArchitectureTranslator;
+import edu.tum.cs.cadmos.analysis.Node;
+import edu.tum.cs.cadmos.language.cadmos.Component;
 
 public class CadmosXtextModelSelectionChangedListener implements
 		ISelectionChangedListener {
@@ -36,10 +41,27 @@ public class CadmosXtextModelSelectionChangedListener implements
 					return;
 				}
 				final ICompositeNode rootNode = parseResult.getRootNode();
-				final ILeafNode node = NodeModelUtils.findLeafNodeAtOffset(
+				final ILeafNode leafNode = NodeModelUtils.findLeafNodeAtOffset(
 						rootNode, selection.getOffset());
-				final EObject selectedObject = NodeModelUtils
-						.findActualSemanticObjectFor(node);
+				EObject selectedObject = NodeModelUtils
+						.findActualSemanticObjectFor(leafNode);
+
+				// FIXME: The following is for debugging only
+				if (!(selectedObject instanceof Component)) {
+					selectedObject = EcoreUtil2.getContainerOfType(
+							selectedObject, Component.class);
+					if (selectedObject == null) {
+						return;
+					}
+				}
+				final Component component = (Component) selectedObject;
+				final ArchitectureTranslator translator = new ArchitectureTranslator(
+						component);
+				long t = System.nanoTime();
+				final Node node = translator.translate();
+				t = System.nanoTime() - t;
+				System.out.println(node);
+				System.out.println((t / 1000) / 1000.0 + "ms");
 			}
 		});
 	}
