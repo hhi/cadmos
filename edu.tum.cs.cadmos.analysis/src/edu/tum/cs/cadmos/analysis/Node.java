6ca8cfa830d0eba9b6d3560cbd96986ee285ca36
wolfgang.schwitzer@gmail.com <wolfgang.schwitzer@gmail.com@824f5622-b1fa-dc22-659f-56585b84b2b6>
2012-09-05 16:43:33 +0000

    Added id-caching, lazy creation of lists, references and references-by.

--- edu.tum.cs.cadmos.analysis/src/edu/tum/cs/cadmos/analysis/Node.java
+++ edu.tum.cs.cadmos.analysis/src/edu/tum/cs/cadmos/analysis/Node.java
@@ -1,6 +1,7 @@
 package edu.tum.cs.cadmos.analysis;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,7 +15,24 @@ public class Node implements Iterable<Node> {
 	private final Node parent;
 	private final EObject semanticObject;
 	private final int index;
-	private final List<Node> children = new ArrayList<>();
+
+	/**
+	 * List of child nodes, lazily created by first child node in
+	 * {@link #Node(Node, EObject, int)}.
+	 */
+	private List<Node> children;
+
+	/** List of referenced nodes, lazily created in {@link #addReference(Node)}. */
+	private List<Node> references;
+
+	/**
+	 * List of nodes that reference this node, lazily created by first
+	 * referencing node in {@link #addReference(Node)}.
+	 */
+	private List<Node> referencedBy;
+
+	/** Caches this node's id, initialized in {@link #getId()}. */
+	private String id;
 
 	public Node(Node parent, EObject semanticObject, int index) {
 		Assert.assertNotNull(semanticObject, "semanticObject");
@@ -23,6 +41,9 @@ public class Node implements Iterable<Node> {
 		this.semanticObject = semanticObject;
 		this.index = index;
 		if (parent != null) {
+			if (parent.children == null) {
+				parent.children = new ArrayList<>();
+			}
 			Assert.assertNotContainedIn(this, parent.children, "this",
 					"parent.children");
 			parent.children.add(this);
@@ -34,12 +55,19 @@ public class Node implements Iterable<Node> {
 	}
 
 	public String getId() {
-		final String localId = ModelUtils.getEObjectName(getSemanticObject())
-				+ "[" + getIndex() + "]";
-		if (parent == null) {
-			return localId;
+		if (id == null) {
+			if (parent == null) {
+				id = getLocalId();
+			} else {
+				id = parent.getId() + "." + getLocalId();
+			}
 		}
-		return parent.getId() + "." + localId;
+		return id;
+	}
+
+	private String getLocalId() {
+		return ModelUtils.getEObjectName(getSemanticObject()) + "["
+				+ getIndex() + "]";
 	}
 
 	public EObject getSemanticObject() {
@@ -51,6 +79,9 @@ public class Node implements Iterable<Node> {
 	}
 
 	public Node findChild(EObject semanticObject, int index) {
+		if (children == null) {
+			return null;
+		}
 		for (final Node child : children) {
 			if (child.getSemanticObject() == semanticObject
 					&& child.getIndex() == index) {
@@ -61,13 +92,13 @@ public class Node implements Iterable<Node> {
 	}
 
 	public Node getFirst() {
-		Assert.assertTrue(!children.isEmpty(),
+		Assert.assertTrue(children != null,
 				"Cannot get first element: 'children' are emtpy");
 		return children.get(0);
 	}
 
 	public Node getLast() {
-		Assert.assertTrue(!children.isEmpty(),
+		Assert.assertTrue(children != null,
 				"Cannot get last element: 'children'  are emtpy");
 		return children.get(children.size() - 1);
 	}
@@ -83,7 +114,49 @@ public class Node implements Iterable<Node> {
 
 	@Override
 	public Iterator<Node> iterator() {
+		if (children == null) {
+			return Collections.EMPTY_LIST.iterator();
+		}
 		return children.iterator();
+	}
+
+	public void addReference(Node ref) {
+		Assert.assertNotNull(ref, "ref");
+		if (references == null) {
+			references = new ArrayList<>();
+		}
+		references.add(ref);
+		if (ref.referencedBy == null) {
+			ref.referencedBy = new ArrayList<>();
+		}
+		ref.referencedBy.add(this);
+	}
+
+	public List<Node> getReferences() {
+		if (references == null) {
+			return Collections.EMPTY_LIST;
+		}
+		return Collections.unmodifiableList(references);
+	}
+
+	public Node getFirstReference() {
+		Assert.assertTrue(references != null,
+				"Cannot get first element: 'references' are emtpy");
+		return references.get(0);
+
+	}
+
+	public Node getLastReference() {
+		Assert.assertTrue(references != null,
+				"Cannot get last element: 'references' are emtpy");
+		return references.get(references.size() - 1);
+	}
+
+	public List<Node> getReferencedBy() {
+		if (referencedBy == null) {
+			return Collections.EMPTY_LIST;
+		}
+		return Collections.unmodifiableList(referencedBy);
 	}
 
 	@Override
@@ -99,35 +172,44 @@ public class Node implements Iterable<Node> {
 			activeLevels.add(false);
 		}
 		for (int i = 0; i < indent - 1; i++) {
-			if (activeLevels.get(i)) {
-				s.append("│");
-			} else {
-				s.append(" ");
-			}
-			s.append(" ");
+			s.append(activeLevels.get(i) ? "│ " : "  ");
 		}
 		if (indent > 0) {
-			if (node.isLast()) {
-				s.append("└─");
-			} else {
-				s.append("├─");
-			}
+			s.append(node.isLast() ? "└─" : "├─");
 		}
 		s.append("o ");
-		final String text = node.getSemanticObject().toString()
-				.replace("edu.tum.cs.cadmos.language.cadmos.impl.", "")
-				.replace("Impl@", "@").replaceAll("@[a-f0-9]+", "");
-		s.append(text);
-		s.append(" [");
-		s.append(node.getIndex());
-		s.append("]");
+		// TODO: remove this alternative node text, which is useful for
+		// debugging at the moment.
+		//
+		// String text = node.getSemanticObject().toString();
+		// text = text.replace("edu.tum.cs.cadmos.language.cadmos.impl.", "");
+		// text = text.replace("Impl@", "[" + node.getIndex() + "]@");
+		// text = text.replaceAll("@[a-f0-9]+", "");
+		// s.append(text);
+		s.append(node.getLocalId());
+		appendLocalIdList(s, "references", node.getReferences());
+		appendLocalIdList(s, "referencedBy", node.getReferencedBy());
 		activeLevels.set(indent, true);
 		for (final Node child : node) {
 			s.append("\n");
-			if (child.isLast()) {
-				activeLevels.set(indent, false);
-			}
+			activeLevels.set(indent, !child.isLast());
 			toString(child, indent + 1, s, activeLevels);
 		}
+	}
+
+	private static void appendLocalIdList(StringBuilder s, String name,
+			List<Node> list) {
+		if (list.isEmpty()) {
+			return;
+		}
+		s.append("(");
+		s.append(name);
+		s.append(": ");
+		s.append(list.get(0).getLocalId());
+		for (int i = 1; i < list.size(); i++) {
+			s.append(", ");
+			s.append(list.get(i).getLocalId());
+		}
+		s.append(") ");
 	}
 }
