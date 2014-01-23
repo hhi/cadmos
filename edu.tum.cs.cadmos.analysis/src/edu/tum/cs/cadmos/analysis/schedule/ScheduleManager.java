385b35b73bdcac96da5614aa50f0ae0569fe0e57
nvpopa@gmail.com <nvpopa@gmail.com@824f5622-b1fa-dc22-659f-56585b84b2b6>
2014-01-23 17:00:56 +0000

    Implemented the generation of the schedule script.

--- edu.tum.cs.cadmos.analysis/src/edu/tum/cs/cadmos/analysis/schedule/ScheduleManager.java
+++ edu.tum.cs.cadmos.analysis/src/edu/tum/cs/cadmos/analysis/schedule/ScheduleManager.java
@@ -1,5 +1,11 @@
 package edu.tum.cs.cadmos.analysis.schedule;
 
+import java.io.File;
+
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+
 import edu.tum.cs.cadmos.analysis.architecture.model.Edge;
 import edu.tum.cs.cadmos.analysis.architecture.model.Vertex;
 import edu.uci.ics.jung.graph.DirectedSparseMultigraph;
@@ -28,6 +34,20 @@ public class ScheduleManager {
 	public void schedule() {
 		if (!readyToSchedule()) {
 			return;
+		}
+		final IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace()
+				.getRoot();
+		final IPath location = workspaceRoot.getLocation();
+		File outputDirectory = new File(location.toFile(), "SMT Scripts");
+		if (!outputDirectory.exists()) {
+			outputDirectory.mkdir();
+		}
+
+		String generatedFileName = "";
+		if (outputDirectory.isDirectory()) {
+			generatedFileName = ScheduleSMTGenerator.doGenerate(
+					outputDirectory, softwareComponentDFG,
+					processingComponentDFG);
 		}
 
 	}
